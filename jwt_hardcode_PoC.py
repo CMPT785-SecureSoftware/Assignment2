593b25c3e0bd1ae0d583046ef203dddb18e6af98
jerilkbestin <jerilk.bestin@gmail.com>
2025-03-21 17:19:09 -0700

    modified:   jwt_hardcode_PoC.py

--- jwt_hardcode_PoC.py
+++ jwt_hardcode_PoC.py
@@ -1,6 +1,5 @@
 import jwt 
 import pickle
-import os
 
 SECRET_KEY="secret_key"
 token = jwt.encode({ "username": "admin1" }, SECRET_KEY, algorithm="HS256")
@@ -9,5 +8,5 @@ obfuscate2 = obfuscate1.hex()
 obfuscate3 = obfuscate2[len(obfuscate2)//2:] + obfuscate2[:len(obfuscate2)//2]
 
 # put it as an environment variable
-os.environ["TOKEN"] = str(obfuscate3)
+print("Obfuscated Token (set in env):", obfuscate3)
 
